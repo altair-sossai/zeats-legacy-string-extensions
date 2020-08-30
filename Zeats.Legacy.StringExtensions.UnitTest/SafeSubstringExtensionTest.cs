e804b53450b7f601c20369d68cd34b050240dd86
Altair Sossai <altair_sossai@outlook.com>
2020-08-30 09:38:12 -0300

    SafeSubstringExtension

---
+++ Zeats.Legacy.StringExtensions.UnitTest/SafeSubstringExtensionTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Zeats.Legacy.StringExtensions.UnitTest
+{
+    [TestClass]
+    public class SafeSubstringExtensionTest
+    {
+        [TestMethod]
+        public void SafeSubstring()
+        {
+            Assert.IsNull(((string) null).SafeSubstring(0, 10));
+            Assert.AreEqual(string.Empty, string.Empty.SafeSubstring(0, 10));
+
+            Assert.AreEqual("12345 7890", "12345 7890".SafeSubstring(0, 9999));
+            Assert.AreEqual("1", "12345 7890".SafeSubstring(0, 1));
+            Assert.AreEqual("12345", "12345 7890".SafeSubstring(0, 5));
+            Assert.AreEqual("12345 ", "12345 7890".SafeSubstring(0, 6));
+            Assert.AreEqual("12345 7890", "12345 7890".SafeSubstring(0, 10));
+        }
+    }
+}
---
+++ Zeats.Legacy.StringExtensions/SafeSubstringExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace Zeats.Legacy.StringExtensions
+{
+    public static class SafeSubstringExtension
+    {
+        public static string SafeSubstring(this string text, int start, int length)
+        {
+            if (string.IsNullOrEmpty(text))
+                return text;
+
+            return text.Length <= start ? ""
+                : text.Length - start <= length ? text.Substring(start)
+                : text.Substring(start, length);
+        }
+    }
+}
